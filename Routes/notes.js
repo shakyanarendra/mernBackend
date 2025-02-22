200d1d054e5cc17e158ecf243510afbb46455677
shakyanarendra <133335362+shakyanarendra@users.noreply.github.com>
2025-02-22 15:55:24 +0530

    Update notes.js

--- Routes/notes.js
+++ Routes/notes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const NotesController = require("../Controllers/NotesController");
+const NotesController = require("../controller/NotesController");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
