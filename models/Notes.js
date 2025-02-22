62e086126da7bf5f6c68a315a2b5e0f55412cfe2
shakyanarendra <133335362+shakyanarendra@users.noreply.github.com>
2025-02-22 15:44:54 +0530

    Update Notes.js

--- models/Notes.js
+++ models/Notes.js
@@ -1,27 +1,28 @@
-const mongoose=require('mongoose');
-const NoteSchema=mongoose.Schema({
-    fileName:{
-        type: String,
-        required:true
-    },
-    fileDescription:{
-        type: String,
-        required:true
-    },
-    tags:{
-        type: String,
-        required:true
-    },
-    files:{
-        type: String,
-        required:true
-    },
-    uploadedBy:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
-    }
+const mongoose = require("mongoose");
 
-    
-})
-module.exports=mongoose.model("Notes",NoteSchema);
+const NoteSchema = mongoose.Schema({
+    fileName: {
+        type: String,
+        required: true,
+    },
+    fileDescription: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: String,
+        required: true,
+    },
+    files: {
+        type: String,
+        required: true,
+    },
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+});
+
+module.exports = mongoose.model("Notes", NoteSchema);
