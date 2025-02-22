73d74e212df997fcac78214e52df3352115c1eb1
shakyanarendra <133335362+shakyanarendra@users.noreply.github.com>
2025-02-22 15:57:41 +0530

    Update NotesController.js

--- controller/NotesController.js
+++ controller/NotesController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const Notes = require("../Models/Notes");
+const Notes = require("../models/Notes");
 const multer = require("multer");
 const path = require("path");
 
