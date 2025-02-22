ab64ac5cbb854f5987293245de75c26b8ed043f9
shakyanarendra <133335362+shakyanarendra@users.noreply.github.com>
2025-02-22 15:40:45 +0530

    Update Authcontroller.js
    chipka dia

--- controller/Authcontroller.js
+++ controller/Authcontroller.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const User = require("../models/user.js");
-// const bcrypt = require("bcrypt");
-const bcrypt = require('bcryptjs');
+const User = require("../Models/User");
+const bcrypt = require("bcrypt");
 const multer = require("multer");
 const cloudinary = require("cloudinary");
-
 
 dotenv.config();
 
@@ -16,83 +14,80 @@ var upload = multer({
     storage: storage
 });
 
+//Signup Route
+const signup = async (req, res) => {
+    try {
+        const { firstName, lastName, userBio, userEmail, userMobile, userName } = req.body;
 
-// sign up
+        // If current user exists
 
-const signup=async(req,res)=>{
-    try{
-        const {firstName,lastName,userBio,userEmail,userMobile,userName}=req.body;
-        const existingUser=await User.findOne({userEmail});
-        if(existingUser)
-              {
-                res.status(401).send("user allready exist");
-              }
+        const existingUser = await User.findOne({ userEmail });
+        if (existingUser) {
+            res.status(401).send("User Already Exists with this email");
+        }
 
-            if(!req.file){
-                return res.status(400).json({error:"No profile image provided"});
-            }
-            const result=await cloudinary.uploader.upload(req.file.path);
-            console.log(result);
+        // Check if file is provided
+        if (!req.file) {
+            return res.status(400).json({ error: "No Profile Image Provided" });
+        }
 
-            const password=req.body.userPassword;
-            const saltRounds=10;
-            const salt=await bcrypt.genSalt(saltRounds);
-            const encryptedPassword=await bcrypt.hash(password,salt);
-            console.log("request body:",req.body);
+        const result = await cloudinary.uploader.upload(req.file.path);
+        console.log(result);
 
-            const newUser = new User({
-                firstName,
-                lastName,
-                userBio,
-                userEmail,
-                userMobile,
-                userName,
-                userPassword: encryptedPassword,
-                profileImage: result.secure_url
-            });
-            await newUser.save();
-            
+        const password = req.body.userPassword;
+        const saltRounds = 10;
 
-            return res.status(200).json({
-                status: "Ok",
-                user: newUser
-            });
-    }      
-      
-    catch(error){
+        const salt = await bcrypt.genSalt(saltRounds);
+
+        const encryptedPassword = await bcrypt.hash(password, salt);
+        console.log("Request Body: ", req.body);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            userBio,
+            userEmail,
+            userMobile,
+            userName,
+            userPassword: encryptedPassword,
+            profileImage: result.secure_url
+        });
+
+        await newUser.save();
+
+        return res.status(200).json({
+            status: "Ok",
+            user: newUser
+        });
+
+    } catch (error) {
         res.status(400).json({ error: error.message });
         console.log(error);
     }
 };
 
-const login=async (req,res)=>{
-    try{
-        const { userEmail,userPassword}=req.body;
-        const user= await User.findOne({userEmail});
-        if(user){
-            const passwordMatch=await bcrypt.compare(userPassword,user.userPassword);
-            if(passwordMatch){
+const login = async (req, res) => {
+    try {
+        const { userEmail, userPassword } = req.body;
+        // console.log(userEmail);
+
+        const user = await User.findOne({ userEmail });
+
+        if (user) {
+            const passwordMatch = await bcrypt.compare(userPassword, user.userPassword);
+            if (passwordMatch) {
                 return res.json(user);
+            } else {
+                return res.json({ status: "Error", getUser: false })
             }
-            else{
-                return res.josn({status:"Error",getUser:false});
-            }
+        } else {
+            return res.json({ status: "Error", getUser: false });
         }
-        else{
-            return res.json({status:"Error",getUser:false});
-        }
-    }
-    catch(error){
-         res.status(400).json({error:error.message});
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 };
 
 
-  
-
-module.exports={signup,login};
-
-
-
-
-
+module.exports = { signup, login };
